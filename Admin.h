0809abc6a0b5c296a8bef695d736252d6a383abb
Jack Hill <jackhill2104@gmail.com>
2022-05-11 16:45:06 +0930

    Formatting

--- Admin.h
+++ Admin.h
@@ -6,14 +6,14 @@
 
 class Admin : public Profile {
     public:
-    void createTeacher();
-    void removeTeacher(std::string ID);
-    void createStudent();
-    void removeStudent(std::string ID);
-    void createAdmin();
-    void removeAdmin(std::string ID);
-    std::string printTitle();
-    std::string constructEmail();
+        void createTeacher();
+        void removeTeacher(std::string ID);
+        void createStudent();
+        void removeStudent(std::string ID);
+        void createAdmin();
+        void removeAdmin(std::string ID);
+        std::string printTitle();
+        std::string constructEmail();
 };
 
 #endif
--- Student.h
+++ Student.h
@@ -7,21 +7,22 @@
 
 class Student : public Profile {
     private:
-    int courseCount;
-    Course* courses;
+        int courseCount;
+        Course* courses;
+
     public:
-    Student();
-    Student(int courseCount, std::string name, std::string password, int schoolID);
-    ~Student();
-    void printCourses();
-    void printTimetable();
-    void printReport();
-    void enrol(std::string courseName);
-    void unenrol(std::string courseName);
-    void enrol(int courseName);
-    void unenrol(int courseID);
-    void printTitle();
-    std::string constructEmail();
+        Student();
+        Student(int courseCount, std::string name, std::string password, int schoolID);
+        ~Student();
+        void printCourses();
+        void printTimetable();
+        void printReport();
+        void enrol(std::string courseName);
+        void unenrol(std::string courseName);
+        void enrol(int courseName);
+        void unenrol(int courseID);
+        void printTitle();
+        std::string constructEmail();
 };
 
 #endif
