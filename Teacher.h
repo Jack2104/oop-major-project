afca5c4059d820995c66c85745e18f5aa19d91f8
ethanarxher <a1771285@student.adelaide.edu.au>
2022-05-11 15:41:28 +0930

    teacher_header

--- Teacher.h
+++ Teacher.h
@@ -3,13 +3,16 @@
 
 #include <string>
 #include "Profile.h"
+#include "Course.h"
 
 class Teacher : public Profile {
     public:
-
-    
+    Teacher();
+    Teacher(int courseCount, string name, string password, int schoolID);
+    ~Teacher();
     private:
-        
+    int courseCount;
+    Course* teachableCourses;         
 };
 
 #endif
