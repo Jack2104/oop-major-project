c9eeae33401229e9429f0190616834fb4b776ad1
ethanarxher <a1771285@student.adelaide.edu.au>
2022-05-11 16:22:10 +0930

    teacherheader

--- Teacher.h
+++ Teacher.h
@@ -4,15 +4,22 @@
 #include <string>
 #include "Profile.h"
 #include "Course.h"
+#include "Assignment.h"
 
 class Teacher : public Profile {
     public:
     Teacher();
-    Teacher(int courseCount, string name, string password, int schoolID);
+    Teacher(int courseCount, std::string name, std::string password, int schoolID);
     ~Teacher();
+    void createCourse();
+    void deleteCourse();
+    void printTitle();
+    std::string constructEmail();
+    void grade(Assignment* Assignment);
+    Assignment createAssignment();
     private:
     int courseCount;
-    Course* teachableCourses;         
+    Course* teachableCourses;
 };
 
 #endif
