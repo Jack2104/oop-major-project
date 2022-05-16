25569dd9b75f1052860d24b29f41576bc441261c
ethanarxher <a1771285@student.adelaide.edu.au>
2022-05-16 12:03:59 +0930

    Not finished: Course.cpp

--- Course.h
+++ Course.h
@@ -6,10 +6,29 @@
 
 class Course {
     private:
+        string name;
+        int courseID;
+        int assignmentCount;
+        Assignment* assignments;
+        string day;
 
-    
     public:
-        
+        Course();
+        Course(string name, int courseID, int assignmentCount);
+        ~Course();
+        Assignment* getAssignment(string name);
+        void addAssignment(Assignment* Assignment);
+        void createAssignment();
+        void removeAssignment(string name);
+        float calculateGrade();
+        char percentageToLetterGrade(float percentage);
+        void printGrade();
+        string getName();
+        int getCourseID();
+        void getDay();
+        void setName(string name);
+        void setCourseID(int courseID);
+
 };
 
 #endif
