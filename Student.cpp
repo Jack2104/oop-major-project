82719d183937b88c828801c84e5b0eadf50f0236
ethanarxher <a1771285@student.adelaide.edu.au>
2022-05-16 13:22:48 +0930

    Not finished

--- Student.cpp
+++ Student.cpp
@@ -3,4 +3,30 @@
 
 #include "Student.h"
 
+using namespace std;
 
+Student::Student() {
+    courseCount = 0;
+    name = "?";
+    password = "?";
+    schoolID = 0;
+};
+
+Student::Student(int courseCount, string name, string password, int schoolID) {
+    this->courseCount = courseCount;
+    this->name = name;
+    this->password = password;
+    this->schoolID = schoolID;
+};
+
+Student::~Student() {};
+
+void Student::printCourses() {
+    for (int i = 0 ; i < courseCount ; i++) {
+        cout << "Course: " << courses[i].getCourseID() << " " << courses[i].getName() << endl;
+    }
+};
+
+void Student::enrol(string courseName) {
+    
+};
