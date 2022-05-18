9daf9370010e6dcf00bfb62bf83dd78010765145
Jack Hill <jackhill2104@gmail.com>
2022-05-18 14:17:44 +0930

    misc

---
+++ CourseInputTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#include "Course.h"
+#include "Assignment.h"
+
+int main() {
+    Course assignment("Major practical", 100, "A big project");
+    
+    std::cout << "Testing createAssignment()...";
+    course.createAssignment();
+
+    return 0;
+}
