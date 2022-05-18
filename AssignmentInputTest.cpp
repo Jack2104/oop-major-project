19d279bb42d80330d5834a325a2c951bc439e1c9
Jack Hill <jackhill2104@gmail.com>
2022-05-18 14:06:53 +0930

    Misc

---
+++ AssignmentInputTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+#include "Assignment.h"
+
+int main() {
+    Assignment assignment("Major practical", 100, "A big project");
+    assignment.setGrade(100);
+
+    std::cout << "Testing printAssignmentInfo()..."
+`
+    assignment.printAssignmentInfo();
+
+    return 0;
+}
